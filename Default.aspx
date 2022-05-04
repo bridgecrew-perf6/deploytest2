3cb5373f7a8c144b18a985290bc8074a2065bcc9
soo <tndus@sports2i.com>
2022-05-04 16:37:08 +0900

    초기 커밋.

---
+++ Default.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="TestProject.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+	<script type="text/javascript" charset="utf-8" src="/resources/js/jquery-1.12.4.min.js"></script>
+    <script type="text/javascript">
+        $(document).ready(function () {
+            // 데이터
+            getData(1, 2020);
+        });
+
+        // 데이터 가져오기
+        function getData(leId, seasonId) {
+            var request = $.ajax({
+                type: "post"
+                , url: "/ws/Common.asmx/GetTest"
+                , dataType: "json"
+                , data: {
+                    leId: leId
+                    , seasonId: seasonId
+                }
+                , async: false
+                , error: function (e) {
+                    console.log(e);
+                }
+            });
+
+            request.done(function (data) {
+                console.log(data);
+            });
+        }
+    </script>
+</head>
+<body>
+        <div>
+        </div>
+</body>
+</html>
---
+++ Engine/CommonAction.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Practices.EnterpriseLibrary.Common.Configuration;
+using Microsoft.Practices.EnterpriseLibrary.Data;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Linq;
+using System.Web;
+
+namespace TestProject.Engine
+{
+    public class CommonAction
+    {
+        private Database kboDB = EnterpriseLibraryContainer.Current.GetInstance<Database>("kbo_db2");
+
+        /// <summary>
+        /// 참고
+        /// </summary>
+        public JObject GetTest(string leId, string seasonId)
+        {
+            JObject result = new JObject();
+            try
+            {
+                DbCommand cmd = kboDB.GetStoredProcCommand("PROC_KBO_DB2_PLAYER_LE_SEASON_TOP5");
+                kboDB.AddInParameter(cmd, "@LE_ID", DbType.Int16, leId);
+                kboDB.AddInParameter(cmd, "@SEASON_ID", DbType.Int16, seasonId);
+
+                DataSet dsData = kboDB.ExecuteDataSet(cmd);
+
+                DataRow[] drData = dsData.Tables[0].Select();
+
+                JArray list = new JArray();
+
+                foreach(DataRow item in drData)
+                {
+                    JObject obj = new JObject();
+                    obj.Add(new JProperty("SEASON_ID", item["SEASON_ID"]));
+                    obj.Add(new JProperty("P_NM", item["P_NM"]));
+                    obj.Add(new JProperty("T_ID", item["T_ID"]));
+                    list.Add(obj);
+                }
+
+                result.Add(new JProperty("list", list));
+            }
+            catch(Exception ex)
+            {
+
+            }
+            return result;
+        }
+    }
+}
